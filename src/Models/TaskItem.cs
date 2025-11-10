ea3f4b0b73194ce024e417bc09bbfe4e41109d0f
Taha Alani <taha.alani@epitech.eu>
2025-11-10 15:53:09 +0100

    feat: step 1

--- src/Models/TaskItem.cs
+++ src/Models/TaskItem.cs
@@ -2,6 +2,7 @@ namespace TodoListApp.Models;
 
 public class TaskItem
 {
+    public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public bool IsCompleted { get; set; } = false;
 }
